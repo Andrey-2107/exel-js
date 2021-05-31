3ed24abfd4688532cb2e80eef339445259c6abfc
a.morgan <andrey.morgan2107@gmail.com>
2021-05-31 17:02:44 +0300

    Add config for webpack

---
+++ .eslintrs.js
@@ -0,0 +1,17 @@
+module.exports = {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        configFile: "./babel.config.json"
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    extends: ["eslint:recommended", "google"],
+    rules: {
+        "comma-dangle": "off",
+        "require-jsdoc": "off"
+
+    }
+};
